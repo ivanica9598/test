c7a4fdd3c8ead5254dff15923cbbedcc42d3b6b2
tresnja <ivana.milivojevic@elfak.rs>
2022-07-06 16:26:01 +0200

    Add requirements.txt

--- pages/Run_on_video.py
+++ pages/Run_on_video.py
@@ -10,4 +10,6 @@ class VideoProcessor:
 	def recv(self, frame):
 		return frame
 
-webrtc_streamer(key="key", video_processor_factory=VideoProcessor)
+webrtc_streamer(key="key", video_processor_factory=VideoProcessor, rtc_configuration=RTCConfiguration(
+					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+					))
